d5b4b82c3eaf645d81ec012f998d916209dbf7cd
b19karhj <sc2rena@gmail.com>
2024-10-24 18:32:46 +0200

    Update app.js
    Google maps and marker added to app.js and fixed spelling misstake in index.html

--- src/app.ts
+++ src/app.ts
@@ -7,25 +7,41 @@ const GOOGLE_API_KEY = process.env.API_KEY; //Add your google API KEY
 
 function loadGoogleMapsAPI() {
   const script = document.createElement("script");
-  script.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_API_KEY}&loading=async&region=JP&language=ja&callback=initMap`;
+  script.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_API_KEY}`;
   script.async = true;
   script.defer = true;
-}
 
+  document.head.appendChild(script);
+}
+declare var google: any;
+type GoogleGeoCodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
-
   axios
-    .get(
+    .get<GoogleGeoCodingResponse>(
       `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
         enteredAddress
       )}&key=${GOOGLE_API_KEY}`
     )
     .then((response) => {
-      console.log(response);
+      if (response.data.status !== "OK") {
+        throw new Error("Could not fetch location");
+      }
+      const coordinates = response.data.results[0].geometry.location;
+      console.log(coordinates);
+      const map = new google.maps.Map(document.getElementById("map"), {
+        center: coordinates,
+        zoom: 8,
+      });
+
+      new google.maps.Marker({ position: coordinates, map: map });
     })
     .catch((err) => {
+      alert(err.message);
       console.log(err);
     });
 }
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     publicPath: "/dist/",
   },
+  devtool: "source-map",
   module: {
     rules: [
       {
