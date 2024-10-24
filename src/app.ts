c53f1c5d748efba1c1d27999dd3ca8edf10fe673
b19karhj <sc2rena@gmail.com>
2024-10-24 16:46:06 +0200

    Update app.ts
    Added DOM loading function for Google Maps API key to hide key in .env.
    Added restricted types for response from google maps api,

--- src/app.ts
+++ src/app.ts
@@ -5,31 +5,30 @@ const addressInput = document.getElementById("address")! as HTMLInputElement;
 
 const GOOGLE_API_KEY = process.env.API_KEY; //Add your google API KEY
 
-type GoogleGeoCodingResponse = {
-  results: { geometry: { location: { lat: number; lng: number } } }[];
-  status: "OK" | "ZERO_RESULTS";
-};
+function loadGoogleMapsAPI() {
+  const script = document.createElement("script");
+  script.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_API_KEY}&loading=async&region=JP&language=ja&callback=initMap`;
+  script.async = true;
+  script.defer = true;
+}
 
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
 
   axios
-    .get<GoogleGeoCodingResponse>(
+    .get(
       `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
         enteredAddress
       )}&key=${GOOGLE_API_KEY}`
     )
     .then((response) => {
-      if (response.data.status !== "OK") {
-        throw new Error("Could not fetch location");
-      }
-      const coordinates = response.data.results[0].geometry.location;
+      console.log(response);
     })
     .catch((err) => {
-      alert(err.message);
       console.log(err);
     });
 }
 
 form.addEventListener("submit", searchAddressHandler);
+document.addEventListener("DOMContentLoaded", loadGoogleMapsAPI);
