80ef2963b7533fb0d46b00b71afae917f2ff91f1
b19karhj <sc2rena@gmail.com>
2024-10-24 15:49:23 +0200

    API key added
    Googles api key added. with .env file

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 const form = document.querySelector("form")!;
 const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+const GOOGLE_API_KEY = process.env.API_KEY; //Add your google API KEY
 
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
-
-  //send to google api.
 }
 
 form.addEventListener("submit", searchAddressHandler);
